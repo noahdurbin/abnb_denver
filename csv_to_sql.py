9192cd17ebe227fb47e853b6b877243cc196c708
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2022-12-06 10:38:41 -0700

    Initial commit

---
+++ csv_to_sql.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import glob, os
+from sqlalchemy import create_engine
+for file in glob.glob("*.csv"):
+    df = pd.read_csv(file)
+    
+    # Create SQLAlchemy engine to connect to MySQL Database
+    engine = create_engine("mysql+mysqldb://root:Her0click!>@localhost:3306/airbnb")
+
+    # Convert dataframe to sql table                                   
+    df.to_sql(file[:-4], engine, index=False)
+    
+    print('done')
